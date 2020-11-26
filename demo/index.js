c45b97bd64dade0b1c6dd29f85b4066c1aef4025
freearhey <free.arhey@gmail.com>
2020-11-27 01:36:39 +0300

    wip

--- demo/index.js
+++ demo/index.js
@@ -1,28 +1,38 @@
 const input = document.getElementById('input')
-const image = document.getElementById('image')
 const output = document.getElementById('output')
 
 input.onchange = function (event) {
-  const file = event.target.files[0]
+  const files = event.target.files
 
-  // Display image
-  const reader = new FileReader()
-  reader.onload = event => {
-    image.src = event.target.result
+  output.innerHTML = ''
+
+  for(let file of files) {
+    const box = document.createElement('div')
+    output.appendChild(box)
+    // Display image
+    const reader = new FileReader()
+    reader.onload = event => {
+      const image = new Image()
+      image.src = event.target.result
+      box.appendChild(image)
+    }
+    reader.readAsDataURL(file)
+
+    // Calculate hash
+    pHash.hash(file).then(hash => {
+      const textbox = document.createElement('div')
+      textbox.innerText = `binary: ${hash.toBinary()}
+        hex: ${hash.toHex()}
+        int: ${hash.toInt()}
+      `
+      box.appendChild(textbox)
+    })
   }
-  reader.readAsDataURL(file)
-
-  // Calculate hash
-  pHash.hash(file).then(hash => {
-    output.innerText = `binary: ${hash.toBinary()}
-      hex: ${hash.toHex()}
-      int: ${hash.toInt()}
-    `
-  })
 
   // Compare two images
-  // const files = event.target.files
-  // hasher.compare(files[0], files[1]).then(distance => {
-  //   console.log(distance)
-  // })
+  if(files.length === 2) {
+    pHash.compare(files[0], files[1]).then(distance => {
+      console.log({distance})
+    })
+  }
 }
