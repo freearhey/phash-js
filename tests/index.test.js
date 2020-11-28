253a775902eb8b3745a1d4c8efae3a310b6a01bb
freearhey <free.arhey@gmail.com>
2020-11-28 14:20:37 +0300

    Add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,15 @@
-import Worker from './workerMock.js'
+import Magick from 'wasm-imagemagick'
 import pHash from '../src/index.js'
-import data from './data.json'
+import data from './data/object.json'
+import buffer from './data/buffer.json'
 
-describe('index', () => {
+jest.mock('wasm-imagemagick', () => {
+  return {
+    Call: jest.fn()
+  }
+})
+
+describe('pHash', () => {
   it('can calculate hash from data object', async () => {
     const hash = pHash._calculateHash(data)
     expect(hash.value).toBe('1011010111010110010100100000101100101011001011110011110111111111')
@@ -16,5 +23,16 @@ describe('index', () => {
   it('can return hash as integer', async () => {
     const hash = pHash._calculateHash(data)
     expect(hash.toInt()).toBe(13102750373803672000)
+  })
+
+  it('shoud return valid hash', async () => {
+    Magick.Call.mockImplementation(() => {
+      return [{ buffer }]
+    })
+
+    const file = new File([new ArrayBuffer(1)], 'image.jpg')
+    const hash = await pHash.hash(file)
+
+    expect(hash.value).toBe('1011010111010110010100100000101100101011001011110011110111110111')
   })
 })
